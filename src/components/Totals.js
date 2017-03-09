4ff69063f9c3d2b3387088cb33980d872e803324
Larix Kortbeek <larixk@gmail.com>
2017-03-09 16:21:27 +0100

    Styling tweaks

--- src/components/Totals.js
+++ src/components/Totals.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 export default ({ tweedeKamerCount, eersteKamerCount }) => (
   <div className="party-total">
-    {tweedeKamerCount}
-    <span className="party-total-ratio">
-      {" "}/ 150 ({Math.round(tweedeKamerCount / 1.5)}%)
-    </span>
-    <span className="party-total-label">
-      zetels in de Tweede Kamer
-    </span>
-    <br />
-    {eersteKamerCount}
-    <span className="party-total-ratio">
-      {" "}/ 75 ({Math.round(eersteKamerCount / 0.75)}%)
-    </span>
-    <span className="party-total-label">
-      zetels in de Eerste Kamer
-    </span>
+    <div>
+      {tweedeKamerCount}
+      <span className="party-total-ratio">
+        {" "}/ 150 ({Math.round(tweedeKamerCount / 1.5)}%)
+      </span>
+      <span className="party-total-label">
+        zetels in de Tweede Kamer
+      </span>
+    </div>
+    <div>
+      {eersteKamerCount}
+      <span className="party-total-ratio">
+        {" "}/ 75 ({Math.round(eersteKamerCount / 0.75)}%)
+      </span>
+      <span className="party-total-label">
+        zetels in de Eerste Kamer
+      </span>
+    </div>
   </div>
 )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
+window.addEventListener('touchstart', function() {
+  window.hasTouched = true;
+});
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
