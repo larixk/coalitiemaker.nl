de15d3231ac79553a9e786f04a9a808760340462
Larix Kortbeek <larixk@gmail.com>
2017-03-13 19:17:19 +0100

    New poll data

--- src/parties.js
+++ src/parties.js
@@ -1,37 +1,37 @@
 export default [
   {
     name: "VVD",
-    seats: 24,
+    seats: 27,
     eerste: 13,
     color: "#ff7600"
   },
   {
     name: "PVV",
-    seats: 22,
+    seats: 24,
     eerste: 9,
     color: "#2e89b5",
   },
   {
     name: "CDA",
-    seats: 22,
+    seats: 21,
     eerste: 12,
     color: "#6ab651"
   },
   {
     name: "GroenLinks",
-    seats: 20,
+    seats: 19,
     eerste: 4,
     color: "#82bb00"
   },
   {
     name: "D66",
-    seats: 17,
+    seats: 16,
     eerste: 10,
     color: "#a7e5c0"
   },
   {
     name: "SP",
-    seats: 15,
+    seats: 14,
     eerste: 9,
     color: "#fe2b24"
   },
@@ -49,7 +49,7 @@ export default [
   },
   {
     name: "50PLUS",
-    seats: 5,
+    seats: 4,
     eerste: 2,
     color: "#7f1382"
   },
