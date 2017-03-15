6b44aa3fa7514f7f2b46ccfe586004124bb923ec
Larix Kortbeek <larixk@gmail.com>
2017-03-15 22:13:00 +0100

    Filter calculator

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Party from './Party';
 import Totals from './Totals';
@@ -12,66 +12,87 @@ class Finder extends Component {
     super();
     this.state = {
       expanded: false,
+      excludedParties: [],
     };
   }
   render() {
-    const coalitions = this.state.expanded
-      ? calculator(this.props.parties).filter(
-          coalition => coalition.parties.length <= this.state.expanded,
-        )
-      : [];
-    console.log(coalitions.map(c => c.hash));
-    console.log(coalitions);
+    console.log(this.state.excludedParties);
+    const coalitions = calculator(this.props.parties).filter(coalition =>
+      coalition.parties.every(
+        party => !this.state.excludedParties.includes(party),
+      ));
     let previousCoalitionSize = null;
     return (
       <div className="Finder">
         <h2>Mogelijke coalities</h2>
-        <ul>
-          {coalitions.map(coalition => {
-            let className = 'coalition';
-            if (coalition.parties.length !== previousCoalitionSize) {
-              previousCoalitionSize = coalition.parties.length;
-              className += ' coalition-first-of-group';
-            }
-            return (
-              <li
-                onClick={() => this.props.onClick(coalition.parties)}
-                key={coalition.hash}
-                className={className}
-              >
-                <div className="coalition-parties">
-                  {coalition.parties.map(party => (
-                    <Party key={party.name} party={party} />
-                  ))}
-                </div>
-                <Totals
-                  eersteKamerCount={coalition.eerste}
-                  tweedeKamerCount={coalition.seats}
-                />
-              </li>
-            );
-          })}
-        </ul>
-        <div>
-          {this.state.expanded
-            ? null
-            : <p>
-                Wij hebben alvast alle mogelijke coalities met een meerderheid in Eerste en Tweede Kamer berekend.
-              </p>}
-          {this.state.expanded
-            ? this.state.expanded < 6
-                ? <button
-                    onClick={() =>
-                      this.setState({ expanded: this.state.expanded + 1 })}
+        {this.state.expanded
+          ? <div>
+              <div className="toggles">
+                <p>
+                  Selecteer partijen om coalities mee te nemen in de berekeningen:
+                </p>
+                {this.props.parties.map(party => (
+                  <button
+                    key={party.name}
+                    onClick={() => {
+                      this.setState({
+                        excludedParties: this.state.excludedParties.includes(
+                          party,
+                        )
+                          ? this.state.excludedParties.filter(
+                              excludedParty => excludedParty !== party,
+                            )
+                          : this.state.excludedParties.concat([party]),
+                      });
+                    }}
+                    className={
+                      'toggle' +
+                        (!this.state.excludedParties.includes(party)
+                          ? ' toggle-active'
+                          : '')
+                    }
                   >
-                    Toon coalities met {this.state.expanded + 1} partijen
+                    {party.name}
                   </button>
-                : null
-            : <button onClick={() => this.setState({ expanded: 4 })}>
-                Toon mogelijke coalities
-              </button>}
-        </div>
-
+                ))}
+                <h3>{coalitions.length} coalitie{coalitions.length === 1 ? '' : 's'} met een meerderheid in Eerste en Tweede Kamer gevonden</h3>
+              </div>
+              <ul>
+                {coalitions.map(coalition => {
+                  let className = 'coalition';
+                  if (coalition.parties.length !== previousCoalitionSize) {
+                    previousCoalitionSize = coalition.parties.length;
+                    className += ' coalition-first-of-group';
+                  }
+                  return (
+                    <li
+                      onClick={() => this.props.onClick(coalition.parties)}
+                      key={coalition.hash}
+                      className={className}
+                    >
+                      <div className="coalition-parties">
+                        {coalition.parties.map(party => (
+                          <Party key={party.name} party={party} />
+                        ))}
+                      </div>
+                      <Totals
+                        eersteKamerCount={coalition.eerste}
+                        tweedeKamerCount={coalition.seats}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          : (<div>
+            <p>
+              Wij hebben alvast alle mogelijke coalities met een meerderheid in Eerste en Tweede Kamer berekend.
+            </p><button
+              onClick={() =>
+                this.setState({expanded: true})}
+            >
+              Toon mogelijke coalities
+            </button></div>)}
       </div>
     );
   }
