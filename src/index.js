79396cd26d7a6e732f862a33639c556917d1a174
Larix Kortbeek <larix@plusoneamsterdam.com>
2025-06-11 11:00:48 +0200

    Upgrade React and ReactDOM to version 19, update scheduler dependency, and refactor rendering method to use createRoot.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import "./index.css";
 
-window.addEventListener('touchstart', function() {
+window.addEventListener("touchstart", function () {
   window.hasTouched = true;
 });
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
