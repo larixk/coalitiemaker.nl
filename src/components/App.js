9e057a2c4a32221326fabc1af0d1d898b7672d1a
Niko Halink <nhalink@gmail.com>
2025-09-27 22:51:50 +0200

    Update Ipsos poll

--- src/components/App.js
+++ src/components/App.js
@@ -207,10 +207,10 @@ class App extends Component {
             </li>
             <li>
               <a
-                href="https://www.ipsos-publiek.nl/actueel/ipsos-io-zetelpeiling-cda-voegt-zich-bij-top-twee/"
+                href="https://www.ipsos-publiek.nl/actueel/ipsos-io-zetelpeiling-liberale-partijen-vvd-d66-en-ja21-dicht-bij-elkaar/"
                 target="_blank"
               >
-                Ipsos I&amp;O (03-09-2025)
+                Ipsos I&amp;O (15-09-2025)
               </a>
             </li>
             <li>
