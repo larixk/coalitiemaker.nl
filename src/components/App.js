e412a88ea9d963378aa135ad81f9d2bccff49711
Niko Halink <nhalink@gmail.com>
2025-10-19 02:08:44 +0200

    Update Ipsos poll

--- src/components/App.js
+++ src/components/App.js
@@ -207,10 +207,10 @@ class App extends Component {
             </li>
             <li>
               <a
-                href="https://www.ipsos-publiek.nl/actueel/ipsos-io-zetelpeiling-surplace-voor-de-eindfase/"
+                href="https://www.ipsos-publiek.nl/actueel/ipsos-io-zetelpeiling-d66-groeit-door-na-rtl-debat/"
                 target="_blank"
               >
-                Ipsos I&amp;O (06-10-2025)
+                Ipsos I&amp;O (15-10-2025)
               </a>
             </li>
             <li>
