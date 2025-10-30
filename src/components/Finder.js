a71db8bd1b82b9378f82006cbf28b7245680bf02
Larix Kortbeek <larixk@gmail.com>
2025-10-30 09:30:29 +0100

    Refactor Finder component layout for better responsiveness and update limit state to 4 for coalition selection.

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -11,16 +11,17 @@ class Finder extends Component {
   constructor() {
     super();
     this.state = {
-      limit: 3,
+      limit: 4,
       houseRequirements: [2],
       excludedParties: [],
       minSeats: 76,
     };
   }
   render() {
-    const coalitions = calculator(this.state.houseRequirements, this.state.minSeats)(
-      this.props.parties
-    )
+    const coalitions = calculator(
+      this.state.houseRequirements,
+      this.state.minSeats
+    )(this.props.parties)
       .filter((coalition) =>
         coalition.parties.every(
           (party) => !this.state.excludedParties.includes(party)
@@ -32,56 +33,56 @@ class Finder extends Component {
       <div className="Finder">
         <h2>Automatisch mogelijke coalities berekenen</h2>
         <div className="toggles">
-          <p>Minimaal aantal zetels in een coalitie:</p>
-          <button
-            onClick={() => this.setState({ minSeats: 76 })}
-            className={
-              "toggle" + (this.state.minSeats === 76 ? " toggle-active" : "")
-            }
-          >
-            76 Zetels
-          </button>
-          <button
-            onClick={() => this.setState({ minSeats: 75 })}
-            className={
-              "toggle" + (this.state.minSeats === 75 ? " toggle-active" : "")
-            }
-          >
-            75 Zetels
-          </button>
-          <p>Maximaal aantal partijen in een coalitie:</p>
-          <button
-            onClick={() => this.setState({ limit: 3 })}
-            className={
-              "toggle" + (this.state.limit === 3 ? " toggle-active" : "")
-            }
-          >
-            3 Partijen
-          </button>
-          <button
-            onClick={() => this.setState({ limit: 4 })}
-            className={
-              "toggle" + (this.state.limit === 4 ? " toggle-active" : "")
-            }
-          >
-            4 Partijen
-          </button>
-          <button
-            onClick={() => this.setState({ limit: 5 })}
-            className={
-              "toggle" + (this.state.limit === 5 ? " toggle-active" : "")
-            }
-          >
-            5 Partijen
-          </button>
-          <button
-            onClick={() => this.setState({ limit: 6 })}
-            className={
-              "toggle" + (this.state.limit === 6 ? " toggle-active" : "")
-            }
-          >
-            6 Partijen
-          </button>
+          <div className="toggles-row">
+            <div>
+              <p>Minimaal aantal zetels in een coalitie:</p>
+              <button
+                onClick={() => this.setState({ minSeats: 76 })}
+                className={
+                  "toggle" +
+                  (this.state.minSeats === 76 ? " toggle-active" : "")
+                }
+              >
+                76 Zetels
+              </button>
+              <button
+                onClick={() => this.setState({ minSeats: 75 })}
+                className={
+                  "toggle" +
+                  (this.state.minSeats === 75 ? " toggle-active" : "")
+                }
+              >
+                75 Zetels
+              </button>
+            </div>
+            <div>
+              <p>Maximaal aantal partijen in een coalitie:</p>
+              <button
+                onClick={() => this.setState({ limit: 4 })}
+                className={
+                  "toggle" + (this.state.limit === 4 ? " toggle-active" : "")
+                }
+              >
+                4 Partijen
+              </button>
+              <button
+                onClick={() => this.setState({ limit: 5 })}
+                className={
+                  "toggle" + (this.state.limit === 5 ? " toggle-active" : "")
+                }
+              >
+                5 Partijen
+              </button>
+              <button
+                onClick={() => this.setState({ limit: 6 })}
+                className={
+                  "toggle" + (this.state.limit === 6 ? " toggle-active" : "")
+                }
+              >
+                6 Partijen
+              </button>
+            </div>
+          </div>
           <p>
             Selecteer alle partijen om mee te nemen en wij berekenen alle
             mogelijke coalities met een meerderheid in Tweede en/of Eerste
@@ -113,7 +114,6 @@ class Finder extends Component {
         <h3>
           {coalitions.length} coalitie
           {coalitions.length === 1 ? "" : "s"}{" "}
-          
         </h3>
         <ul>
           {coalitions.map((coalition) => {
